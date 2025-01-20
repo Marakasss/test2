74429be23b020edc5f059c9037609409975c9bf4
Marakasss <152858594+Marakasss@users.noreply.github.com>
2025-01-20 17:01:51 +0000

    Deploying to gh-pages from @ Marakasss/test2@25d2a0f1568c44f352716edd4c0fb50f18d81cd3 🚀

---
+++ page-3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */function r(o,t,n){return`Result: ${o+t+n}`}console.log(r(1,2,3));
+//# sourceMappingURL=page-3.js.map
